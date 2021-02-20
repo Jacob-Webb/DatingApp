781923cc9145105b99adb4f835ceb47412b41ba9
Jacob Webb <jacob@jacob-webb.com>
2021-02-19 21:31:20 -0800

    changed list component to lists component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
+import { ListsComponent } from './lists/lists.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'members', component: MemberListComponent},
   {path: 'members/:id', component: MemberDetailsComponent},
-  {path: 'lists', component: ListComponent},
+  {path: 'lists', component: ListsComponent},
   {path: 'messages', component: MessagesComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -36,7 +36,7 @@ import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ListComponent } from './list/list.component';
+import { ListsComponent } from './lists/lists.component';
 
 @NgModule({
   declarations: [
@@ -48,7 +48,7 @@ import { ListComponent } from './list/list.component';
     MemberListComponent,
     MemberDetailsComponent,
     MessagesComponent,
-    ListComponent
+    ListsComponent
   ],
   imports: [
     BrowserModule,
